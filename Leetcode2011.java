088bd739899466eb310f66fa9a0853a5cac28bbe
Krishnasingh020 <workforkrishnasingh@gmail.com>
2025-10-21 21:05:14 +0530

    Leetcode question no.2011

---
+++ Leetcode2011.java
@@ -0,0 +1,14 @@
+class Leetcode2011 {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        
+        for( String op : operations){
+            if(op.equals("++X") || op.equals("X++")){
+                x++;
+            }else if (op.equals("--X") || op.equals("X--")){
+                x--;
+            }
+        }
+        return x;
+    }
+}
